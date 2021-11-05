c2ff329f58b24470e2b3a9dc37d9a31ad722683e
Guillermo Rodriguez <guillermor@fing.edu.uy>
2021-11-05 13:11:11 -0300

    Adding http api

--- include/eosio/testing_plugin/testing_plugin.hpp
+++ include/eosio/testing_plugin/testing_plugin.hpp
@@ -1,17 +1,44 @@
 #pragma once
 #include <eosio/chain_plugin/chain_plugin.hpp>
 #include <eosio/http_plugin/http_plugin.hpp>
+
+#include <eosio/chain/name.hpp>
+
+#include <fc/io/json.hpp>
+
 #include <appbase/application.hpp>
 
 namespace eosio {
 
-    using namespace appbase;
+    using chain::digest_type;
+    using chain::block_header;
+    using chain::block_id_type;
+    using chain::signature_type;
+    using chain::private_key_type;
+    using chain::signed_block_ptr;
+    using chain::transaction_trace_ptr;
+
+    template<typename KeyType = fc::ecc::private_key_shim>
+    static auto get_private_key(string keyname, string role = "owner") {
+        auto secret = fc::sha256::hash(keyname + role);
+        return private_key_type::regenerate<KeyType>(secret);
+    }
+
+    template<typename KeyType = fc::ecc::private_key_shim>
+    static auto get_public_key(string keyname, string role = "owner") {
+        return get_private_key<KeyType>(keyname, role).get_public_key();
+    }
 
     class testing_plugin : public appbase::plugin<testing_plugin> {
         public:
             testing_plugin();
             virtual ~testing_plugin();
-            
+
+            signed_block_ptr _produce_block(fc::microseconds skip_time);
+
+            void _start_block(fc::time_point block_time);
+            signed_block_ptr _finish_block();
+
             APPBASE_PLUGIN_REQUIRES((chain_plugin)(http_plugin))
             virtual void set_program_options(options_description&, options_description& cfg) override;
             
@@ -21,6 +48,8 @@ namespace eosio {
 
         private:
             std::unique_ptr<class testing_plugin_impl> my;
+            map<account_name, block_id_type> last_produced_block;
+            controller* control;
     };
 
 }
--- testing_plugin.cpp
+++ testing_plugin.cpp
@@ -10,14 +10,95 @@ class testing_plugin_impl {
 testing_plugin::testing_plugin():my(new testing_plugin_impl()){}
 testing_plugin::~testing_plugin(){}
 
+signed_block_ptr testing_plugin::_produce_block(fc::microseconds skip_time) {
+    auto head = control->head_block_state();
+    auto head_time = control->head_block_time();
+    auto next_time = head_time + skip_time;
+
+    if(!control->is_building_block() || control->pending_block_time() != next_time) {
+        _start_block( next_time );
+    }
+
+    auto head_block = _finish_block();
+
+    _start_block( next_time + fc::microseconds(chain::config::block_interval_us));
+    return head_block;
+}
+
+void testing_plugin::_start_block(fc::time_point block_time) {
+    auto head_block_number = control->head_block_num();
+    auto producer = control->head_block_state()->get_scheduled_producer(block_time);
+
+    auto last_produced_block_num = control->last_irreversible_block_num();
+    auto itr = last_produced_block.find(producer.producer_name);
+    if (itr != last_produced_block.end()) {
+        last_produced_block_num = std::max(control->last_irreversible_block_num(), block_header::num_from_id(itr->second));
+    }
+
+    control->start_block(
+        block_time,
+        head_block_number - last_produced_block_num,
+        control->head_block_state()->get_new_protocol_feature_activations());
+
+}
+
+signed_block_ptr testing_plugin::_finish_block() {
+    FC_ASSERT(control->is_building_block(), "must first start a block before it can be finished");
+
+    auto producer = control->head_block_state()->get_scheduled_producer(control->pending_block_time());
+    vector<private_key_type> signing_keys;
+    signing_keys.push_back(get_private_key("eosio", "active"));
+
+    control->finalize_block([&](digest_type d) {
+        std::vector<signature_type> result;
+        result.reserve(signing_keys.size());
+        for (const auto& k: signing_keys)
+            result.emplace_back(k.sign(d));
+
+        return result;
+    });
+
+    control->commit_block();
+    last_produced_block[control->head_block_state()->header.producer] = control->head_block_state()->id;
+
+    return control->head_block_state()->block;
+}
+
 void testing_plugin::set_program_options(options_description&, options_description& cfg) {}
 
 void testing_plugin::plugin_initialize(const variables_map& options) {
-    // FC_LOG_AND_RETHROW()
+    //FC_LOG_AND_RETHROW()
 }
 
 void testing_plugin::plugin_startup() {
    // Make the magic happen
+
+    control = &app().get_plugin<chain_plugin>().chain(); 
+
+    ilog("testing plugin init.");
+    app().get_plugin<http_plugin>().add_api({
+        {"/v1/testing/version", [&](std::string url, std::string body, url_response_callback callback) {
+            try {
+                callback(200, std::string("v0.1a0"));
+            } catch (...) {
+                http_plugin::handle_exception("testing", "version", body, callback);
+            }
+        }}
+    });
+    app().get_plugin<http_plugin>().add_api({
+        {"/v1/testing/skiptime", [&](std::string url, std::string body, url_response_callback callback) {
+            try {
+				fc::variant args = fc::json::from_string(body).get_object();
+                fc::microseconds skip_time(args["time"].as_int64());
+               
+                _produce_block(skip_time);
+
+                callback(200, std::string("ok"));
+            } catch (...) {
+                http_plugin::handle_exception("testing", "skiptime", body, callback);
+            }
+        }}
+    });
 }
 
 void testing_plugin::plugin_shutdown() {
