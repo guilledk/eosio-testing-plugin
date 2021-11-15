688ab3104c6af65f94b6d534e1292f9da85dbffe
Guillermo Rodriguez <guillermor@fing.edu.uy>
2021-11-15 16:38:12 -0300

    Complete refactor to use custom fc lib time point set hook

--- include/eosio/testing_plugin/testing_plugin.hpp
+++ include/eosio/testing_plugin/testing_plugin.hpp
@@ -1,10 +1,9 @@
 #pragma once
-#include <eosio/chain_plugin/chain_plugin.hpp>
 #include <eosio/http_plugin/http_plugin.hpp>
-#include <eosio/producer_plugin/producer_plugin.hpp>
 
 #include <eosio/chain/name.hpp>
 
+#include <fc/time.hpp>
 #include <fc/io/json.hpp>
 
 #include <appbase/application.hpp>
@@ -13,24 +12,16 @@
 using std::string;
 using std::to_string;
 
-namespace eosio {
+using fc::testing_time_provider;
 
-    using chain::digest_type;
-    using chain::block_header;
-    using chain::block_id_type;
-    using chain::signature_type;
-    using chain::private_key_type;
-    using chain::signed_block_ptr;
-    using chain::transaction_trace_ptr;
+namespace eosio {
 
     class testing_plugin : public appbase::plugin<testing_plugin> {
         public:
             testing_plugin();
             virtual ~testing_plugin();
 
-            void _skip_time(fc::microseconds time);
-
-            APPBASE_PLUGIN_REQUIRES((chain_plugin)(http_plugin)(producer_plugin))
+            APPBASE_PLUGIN_REQUIRES((http_plugin))
             virtual void set_program_options(options_description&, options_description& cfg) override;
             
             void plugin_initialize(const variables_map& options);
@@ -39,9 +30,6 @@ namespace eosio {
 
         private:
             std::unique_ptr<class testing_plugin_impl> my;
-            private_key_type eosio_key;
-            producer_plugin* production;
-            controller* control;
     };
 
 }
--- testing_plugin.cpp
+++ testing_plugin.cpp
@@ -11,40 +11,6 @@ class testing_plugin_impl {
 testing_plugin::testing_plugin():my(new testing_plugin_impl()){}
 testing_plugin::~testing_plugin(){}
 
-void testing_plugin::_skip_time(fc::microseconds time) {
-
-    production->pause();
-    
-    if (control->is_building_block())
-        control->abort_block();
-
-    control->start_block(time_point::now() + time, 0);
-
-    control->finalize_block([&](digest_type d) {
-        std::vector<signature_type> result;
-
-        result.push_back(eosio_key.sign(d));
-        
-        return result;
-    });
-    control->commit_block();
-
-    block_state_ptr new_bs = control->head_block_state();
-
-    ilog(
-        "Produced skip time block ${id}... #${n} @ ${t} signed by ${p} [trxs: ${count}, lib: ${lib}, confirmed: ${confs}]",
-        ("p", new_bs->header.producer)
-        ("id", new_bs->id.str().substr(8,16))
-        ("n", new_bs->block_num)
-        ("t", new_bs->header.timestamp)
-        ("count", new_bs->block->transactions.size())
-        ("lib", control->last_irreversible_block_num())
-        ("confs", new_bs->header.confirmed));
-
-    production->resume();
-
-}
-
 void testing_plugin::set_program_options(options_description&, options_description& cfg) {}
 
 void testing_plugin::plugin_initialize(const variables_map& options) {
@@ -52,56 +18,26 @@ void testing_plugin::plugin_initialize(const variables_map& options) {
 }
 
 void testing_plugin::plugin_startup() {
-   // Make the magic happen
-    production = &app().get_plugin<producer_plugin>();
-    control = &app().get_plugin<chain_plugin>().chain();
-
-    eosio_key = private_key_type(
-        string("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"));
 
     app().get_plugin<http_plugin>().add_api({
         {"/v1/testing/version", [&](string url, string body, url_response_callback callback) {
             try {
-                callback(200, string("v0.1a0"));
+                callback(200, string("v0.1a1"));
             } catch (...) {
                 http_plugin::handle_exception("testing", "version", body, callback);
             }
         }}
     });
     app().get_plugin<http_plugin>().add_api({
-        {"/v1/testing/skiptime", [&](string url, string body, url_response_callback callback) {
+        {"/v1/testing/settime", [&](string url, string body, url_response_callback callback) {
             try {
 				fc::variant args = fc::json::from_string(body).get_object();
-                fc::microseconds time(args["time"].as_int64());
                
-                _skip_time(time);
+                testing_time_provider::get().set_time(args["time"].as_int64());
 
                 callback(200, string("ok"));
             } catch (...) {
-                http_plugin::handle_exception("testing", "skiptime", body, callback);
-            }
-        }}
-    });
-    app().get_plugin<http_plugin>().add_api({
-        {"/v1/testing/debug", [&](string url, string body, url_response_callback callback) {
-            try {
-                string ret;
-
-                ret += "head_block_num: " + std::to_string(control->head_block_num()) + "\n";
-                ret += "head_block_id: " + control->head_block_id().str() + "\n";
-                ret += "head_block_time: " + std::to_string(control->head_block_time().sec_since_epoch()) + "\n";
-                ret += "head_block_producer: " + control->head_block_producer().to_string() + "\n";
-                ret += "head_block_prev_id: " + control->head_block_header().previous.str() + "\n";
-
-                //ret += "pending_block_time: " + std::to_string(control->pending_block_time().sec_since_epoch()) + "\n";
-                //ret += "pending_block_producer: " + control->pending_block_producer().to_string();
-
-                ret += "is_building_block: " + std::to_string(control->is_building_block()) + "\n";
-                ret += "is_producing_block: " + std::to_string(control->is_producing_block()) + "\n";
-
-                callback(200, ret);
-            } catch (...) {
-                http_plugin::handle_exception("testing", "debug", body, callback);
+                http_plugin::handle_exception("testing", "settime", body, callback);
             }
         }}
     });
